85e3342e804af5c2fb46b8a07c52570904e4b4f4
gigouni <nicolas.gigou@gmail.com>
2017-06-14 18:18:56 +0200

    Work with the Selenium hub and browers nodes. Add Edge test

--- src/web.js
+++ src/web.js
@@ -10,26 +10,38 @@ test.describe('Search my website on Google', () => {
     var driver;
 
     test.it('should redirect to my website with CHROME', () => {
-        driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build()
+        driver = new webdriver.Builder().usingServer('http://172.17.0.1:5557/wd/hub').withCapabilities(webdriver.Capabilities.chrome()).build()
         driver.get('http://google.com');
         driver.wait(until.titleIs('Google'));
-        driver.wait(until.elementLocated(By.name('q'))).sendKeys('nicolas gigou');
+        driver.wait(until.elementLocated(By.name('q'))).sendKeys('google');
         driver.findElement(By.name('btnG')).click();
-        driver.wait(until.titleIs('nicolas gigou - Recherche Google'));
+        driver.wait(until.titleIs('google - Recherche Google'));
         driver.wait(until.elementLocated(By.tagName('h3.r a'))).click();
-        driver.wait(until.titleIs('Nicolas GIGOU'));
+        driver.wait(until.titleIs('Google'));
         driver.quit();
     });
 
     test.it('should redirect to my website with FIREFOX', () => {
-        driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build()
+        driver = new webdriver.Builder().usingServer('http://172.17.0.1:5555/wd/hub').withCapabilities(webdriver.Capabilities.firefox()).build()
         driver.get('http://google.com');
         driver.wait(until.titleIs('Google'));
-        driver.wait(until.elementLocated(By.name('q'))).sendKeys('nicolas gigou');
+        driver.wait(until.elementLocated(By.name('q'))).sendKeys('google');
         driver.findElement(By.name('btnG')).click();
-        driver.wait(until.titleIs('nicolas gigou - Recherche Google'));
+        driver.wait(until.titleIs('google - Recherche Google'));
         driver.wait(until.elementLocated(By.tagName('h3.r a'))).click();
-        driver.wait(until.titleIs('Nicolas GIGOU'));
+        driver.wait(until.titleIs('Google'));
+        driver.quit();
+    });
+
+    test.it('should redirect to my website with EDGE', () => {
+        driver = new webdriver.Builder().usingServer('http://172.17.0.1:5558/wd/hub').withCapabilities(webdriver.Capabilities.edge()).build()
+        driver.get('http://google.com');
+        driver.wait(until.titleIs('Google'));
+        driver.wait(until.elementLocated(By.name('q'))).sendKeys('google');
+        driver.findElement(By.name('btnG')).click();
+        driver.wait(until.titleIs('google - Recherche Google'));
+        driver.wait(until.elementLocated(By.tagName('h3.r a'))).click();
+        driver.wait(until.titleIs('Google'));
         driver.quit();
     });
 });
