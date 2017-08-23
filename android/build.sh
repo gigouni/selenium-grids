e089902ebb5824c2d893521fad7a895972c5193f
gigouni <nicolas.gigou@gmail.com>
2017-08-23 15:25:42 +0200

    [ANDROID] Add Chrome support comments

--- android/build.sh
+++ android/build.sh
@@ -4,6 +4,13 @@
 
 ## For Chrome driver version, check this
 # https://chromedriver.storage.googleapis.com/2.30/notes.txt, "Supports Chrome" lines
+# ChromeDriver v2.30 : Supports Chrome v58-60
+# ChromeDriver v2.29 : Supports Chrome v56-58
+# ChromeDriver v2.28 : Supports Chrome v55-57
+# ChromeDriver v2.27 : Supports Chrome v54-56
+# ChromeDriver v2.26 : Supports Chrome v53-55
+# ChromeDriver v2.25 : Supports Chrome v53-55
+# ChromeDriver v2.24 : Supports Chrome v52-54
 
 # ===============================================================
 # DO NOT RUN DIRECTLY THIS SCRIPT, JUST RUN THE RUN.SH SCRIPT
