989a79dea85a30fad95723208ae58769623b40d1
gigouni <nicolas.gigou@gmail.com>
2017-08-23 15:24:31 +0200

    [HUB] Add scripts to run and build image

---
+++ hub/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#### Description: Script to automatize the default build process
+#### Author: Nicolas GIGOU <nicolas.gigou [at] gmail.com>
+
+# ===============================================================
+# DO NOT RUN DIRECTLY THIS SCRIPT, JUST RUN THE RUN.SH SCRIPT
+# THE BUILD PHASE WILL BE AUTOMATICALLY DONE
+# ===============================================================
+docker build \
+    -t gigouni/hub-3.4.0-dyprosium \
+    .
---
+++ hub/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#### Description: Script to automatize the default run process
+#### Author: Nicolas GIGOU <nicolas.gigou [at] gmail.com>
+
+./build.sh
+
+docker run \
+    -it \
+    --rm \
+    -p 4444:444 \
+    --net=host \
+    --name selenium-hub \
+    gigouni/hub-3.4.0-dyprosium
+    
+echo "Closing Selenium hub"
